2d0586e25d05928d5da241479d4efb80468502c9
CrazyElfi <al.gaffarova.26@gmail.com>
2019-10-15 01:12:43 +0300

    added 2 blocks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function () {
 
-    $('.list__inner .tab').on('click', function(event) {
+    $('.list__inner .tab, .reviews__tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-        $('.list__inner').find('.tab-item').removeClass('active-tab').hide();
-        $('.list__inner .tabs').find('.tab').removeClass('active');
+        $('.list__inner, .reviews__inner').find('.tab-item, .reviews__content-item').removeClass('active-tab').hide();
+        $('.list__inner .tabs, .reviews__tabs').find('.tab, .reviews__tab').removeClass('active');
         $(this).addClass('active');
         $('#'+id).addClass('active-tab').fadeIn();
         return false;
